b8fcaca48393c7de2d277f0d2add38c24077b842
Alessandro Alex <aleabran1@gmail.com>
2015-02-26 20:24:36 -0300

    correção do arquivo reame.md

---
+++ app/Http/routes (Cópia em conflito de alex-n1 2015-02-20).php
@@ -0,0 +1,12 @@
+<?php
+
+Route::get('/', 'WelcomeController@index');
+
+Route::get('home', 'HomeController@index');
+
+Route::controllers([
+	'auth' => 'Auth\AuthController',
+	'password' => 'Auth\PasswordController',
+]);
+
+Route::get('articles', 'ArticlesController@index');
